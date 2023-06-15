b4ae5656f741742384b18e3c71f81e3f62ff0116
Davi dos Santos <85198192+DaviJS-dev@users.noreply.github.com>
2023-06-15 20:33:44 -0300

    first

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,46 @@
+import { Fragment,useContext } from "react";
+import { Outlet, Link } from "react-router-dom";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+import {ReactComponent as RcStore} from '../../assets/rcStore.svg';
+import { UserContext } from "../../context/user.context";
+import { CartContext } from "../../context/cart.context";
+import './navigation.styles.scss';
+import { signOutUser } from "../../utils/firebase/firebase.utils";
+
+
+
+const Navigation = () => {
+  const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
+
+  return (
+    <Fragment>
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
+          <RcStore className='logo' />
+        </Link>
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
+            SHOP
+          </Link>
+
+          {currentUser ? (
+            <span className='nav-link' onClick={signOutUser}>
+              SIGN OUT
+            </span>
+          ) : (
+            <Link className='nav-link' to='/auth'>
+              SIGN IN
+            </Link>
+          )}
+          <CartIcon />
+        </div>
+        {isCartOpen && <CartDropdown />}
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
